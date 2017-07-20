fde2ac204d3f4c109f2466208df0b7ffa78a02a0
stipanovs <sergiu.stipanov@amdaris.com>
2017-07-20 16:55:33 +0300

    Add project files.

---
+++ MyPatternsStructural/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPatternsStructural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteProxy rmProxy = new RemoteProxy();
+            rmProxy.GetResponseFromServer("dfsaf", DateTime.Today);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyPatternsStructural/RemoteProxy.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyPatternsStructural
+{
+    class RemoteProxy
+    {
+        public string GetResponseFromServer(string input, DateTime date)
+        {
+            string result = string.Empty;
+            
+
+            string requestDate = string.Format("{0}.{1:D2}.{2}", date.Day, date.Month, date.Year);
+            string url = @"https://www.bnm.md/ro/official_exchange_rates?get_xml=1&date=" + requestDate;
+
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+
+                WebResponse response = request.GetResponse();
+
+                Stream dataStream = response.GetResponseStream();
+
+                StreamReader streamReader = new StreamReader(dataStream);
+
+                result = streamReader.ReadToEnd();
+
+                //Console.WriteLine(result);
+
+                streamReader.Close();
+                response.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            // xml parser
+            string xmlString = result;
+
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                //xmlReader.MoveToContent();
+
+                //xmlReader.ReadToFollowing("Valute");
+                //xmlReader.MoveToFirstAttribute();
+                //string ValutaID = xmlReader.Value;
+
+                //output.AppendLine("The valute ID: " + ValutaID);
+
+                //xmlReader.ReadToFollowing("NumCode");
+                //output.AppendLine("     NumCode: " + xmlReader.ReadElementContentAsString());
+
+                //xmlReader.ReadToFollowing("CharCode");
+                //output.AppendLine("     CharCode: " + xmlReader.ReadElementContentAsString());
+
+                //xmlReader.ReadToFollowing("Name");
+                //output.AppendLine("     Name: " + xmlReader.ReadElementContentAsString());
+
+                //xmlReader.ReadToFollowing("Value");
+                //output.AppendLine("     Value: " + xmlReader.ReadElementContentAsString());
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // Узел является элементом.
+
+
+                            Console.Write("" + reader.Name);
+
+                            while (reader.MoveToNextAttribute()) // Чтение атрибутов.
+                                //Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                            Console.WriteLine(">");
+                            break;
+                        case XmlNodeType.Text: // Вывести текст в каждом элементе.
+                            //Console.WriteLine("---value---");
+                            Console.Write(" " + reader.Value);
+                            break;
+                        case XmlNodeType.EndElement: // Вывести конец элемента.
+                            Console.Write("</" + reader.Name);
+                            Console.WriteLine(">");
+                            break;
+                    }
+                }
+
+                string xml_text = output.ToString();
+                 //Console.WriteLine(xml_text);
+            }
+
+
+            // https://habrahabr.ru/post/24673/
+
+            return "";
+        }
+
+    }
+}
